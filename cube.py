6f96559b080a5bd28cf91dd6edaeee681bbbdd0b
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-03-26 10:56:14 +0700

    fix color side

--- cube.py
+++ cube.py
@@ -1,10 +1,11 @@
-# SIDE INDEX
-FRONT = 0
-BACK = 4
-RIGHT = 8
-LEFT = 12
-UP = 16
-DOWN = 20 #START INDEX OF EACH SIDE
+# START INDEX OF EACH SIDE
+UP = 0
+LEFT = 4
+FRONT = 8
+RIGHT = 12
+BACK = 16
+DOWN = 20
+
 
 # COLOR CODE
 WHITE = 0
@@ -15,9 +16,9 @@ BLUE = 4
 GREEN = 5
 
 class Cube:
-    def __init__(self, state="WWWWYYYYOOOORRRRBBBBGGGG"):
+    def __init__(self, state="WWWWOOOOGGGGRRRRBBBBYYYY"):
         #F1234B1234R1234L1234U1234D1234
-        # WWWWYYYYOOOORRRRBBBBGGGG
+        # saved as FBRLUD
         self.state = state # a string og 24 char
         self.h = 0
         self.g = 0
@@ -42,12 +43,11 @@ class Cube:
         # change r13 to d01
         state[RIGHT+1], state[RIGHT+3] = state[DOWN], state[DOWN+1]
         # change d01 to l02
-        state[DOWN], state[DOWN+1] = state[LEFT], state[LEFT+2]
+        state[BACK], state[BACK+1] = state[LEFT], state[LEFT+2]
         # change l02 to f23
         state[LEFT], state[LEFT+2] = tFront2, tFront3
         # convert it back to string
-        self.state = str(state)
-
+        self.state = "".join(state)
 
     def frontCCW(self):
         # convert from string to a list, because a string is immutable
@@ -63,7 +63,7 @@ class Cube:
         # change r13 to f23
         state[RIGHT+1], state[RIGHT+3] = tFront2, tFront3
         # convert it back to string
-        self.state = str(state)
+        self.state = "".join(state)
     
     def upCW(self):
         # convert from string to a list, because a string is immutable
@@ -79,7 +79,7 @@ class Cube:
         # change u01 to r01
         state[UP], state[UP+1] = tright0, tright1
         # convert it back to string
-        self.state = str(state)
+        self.state = "".join(state)
 
     def upCCW(self):
         # convert from string to a list, because a string is immutable
@@ -95,7 +95,7 @@ class Cube:
         # change d01 to r01
         state[UP], state[UP+1] = tright0, tright1
         # convert it back to string
-        self.state = str(state)               
+        self.state = "".join(state)             
 
     def downCW(self):
         # convert from string to a list, because a string is immutable
@@ -111,7 +111,7 @@ class Cube:
         # change d01 to r01
         state[UP], state[UP+1] = tright0, tright1
         # convert it back to string
-        self.state = str(state) 
+        self.state = "".join(state)
 
     def move(self, move):
         if(move == 'F'):
@@ -119,18 +119,22 @@ class Cube:
 
     def printState(self):
         print(f'        +---+---+')
-        print(f'        | {self.state[FRONT]} | {self.state[FRONT+1]} |')
+        print(f'        | {self.state[UP]} | {self.state[UP+1]} |')
         print(f'        +---+---+')
-        print(f'        | {self.state[FRONT+2]} | {self.state[FRONT+3]} |')
+        print(f'        | {self.state[UP+2]} | {self.state[UP+3]} |')
         print(f'+---+---+---+---+---+---+---+---+')
-        print(f'| {self.state[RIGHT]} | {self.state[RIGHT+1]} | {self.state[DOWN]} | {self.state[DOWN+1]} | {self.state[LEFT]} | {self.state[LEFT+1]} | {self.state[UP]} | {self.state[UP+1]} |')
+        print(f'| {self.state[LEFT]} | {self.state[LEFT+1]} | {self.state[FRONT]} | {self.state[FRONT+1]} | {self.state[RIGHT]} | {self.state[RIGHT+1]} | {self.state[BACK]} | {self.state[BACK+1]} |')
         print(f'+---+---+---+---+---+---+---+---+')
-        print(f'| {self.state[RIGHT+2]} | {self.state[RIGHT+3]} | {self.state[DOWN+2]} | {self.state[DOWN+3]} | {self.state[LEFT+2]} | {self.state[LEFT+3]} | {self.state[UP+2]} | {self.state[UP+3]} |')
+        print(f'| {self.state[LEFT+2]} | {self.state[LEFT+3]} | {self.state[FRONT+2]} | {self.state[FRONT+3]} | {self.state[RIGHT+2]} | {self.state[RIGHT+3]} | {self.state[BACK+2]} | {self.state[BACK+3]} |')
         print(f'+---+---+---+---+---+---+---+---+')
-        print(f'        | {self.state[BACK]} | {self.state[BACK+1]} |')
+        print(f'        | {self.state[DOWN]} | {self.state[DOWN+1]} |')
         print(f'        +---+---+')
-        print(f'        | {self.state[BACK+2]} | {self.state[BACK+3]} |')
+        print(f'        | {self.state[DOWN+2]} | {self.state[DOWN+3]} |')
         print(f'        +---+---+')  
 
 mycube = Cube()
 mycube.printState()
+# mycube.frontCW()
+# mycube.printState()
+# mycube.frontCCW()
+# mycube.printState()
