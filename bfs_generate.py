547db23ddb86e47959f6bf35ec1e2d5f7ecd1b79
RavindraWiguna <ravindrawiguna@gmail.com>
2022-04-03 16:26:22 +0700

    generate adjacent side with part1 and 2 (in 1 file is too big)

--- bfs_generate.py
+++ bfs_generate.py
@@ -12,14 +12,14 @@ POS_MOVE = ('F', 'R', 'U', 'B', 'L', 'D', 'FC', 'RC', 'UC', 'BC', 'LC', 'DC')
 
 # WWWWOOOOGGGGRRRRBBBBYYYY
 def bfs_gen():
-    init_cube = Cube("................BBBBYYYY")
+    init_cube = Cube("........GGGGRRRR........")
                       
 
-    heuristic_dict = {}
-    heuristic_dict[init_cube.state] = 0
-
+    arr_hdict = [{}, {}]
+    id_h = 0
+    arr_hdict[id_h][init_cube.state] = 0
     openQue = Queue()
-
+    total_in_dict = 0
     openQue.put((init_cube.state, 0))
     try:
         while openQue.queue:
@@ -30,25 +30,40 @@ def bfs_gen():
                 # generate node based on move
                 move_state = create_cube_state(cur_cube_state, move)
                 
-                # check if this state is in heuristic dict
-                if(move_state in heuristic_dict):
-                    # if we found way to get to this state but faster
-                    if(heuristic_dict[move_state] > depth):
-                        # update the depth
-                        heuristic_dict[move_state] = depth
-                        # put it again in openque
-                        openQue.put((move_state, depth))
+                # boolean indicating if a node has been visited                
+                isEverVisited = False
+                for i in range(2):
+                    if(isEverVisited):
+                        # if this nove ever visited (exist in dict[0])
+                        break
+                    
+                    # check if this state is in [i-th] heuristic dict 
+                    if(move_state in arr_hdict[i]):
+                        # exist in dict, so must have been visited
+                        isEverVisited = True
+                        # if we found way to get to this state but faster
+                        if(arr_hdict[i][move_state] > depth):
+                            # update the depth
+                            arr_hdict[i][move_state] = depth
+                            # put it again in openque
+                            openQue.put((move_state, depth))
+                # continue to next move if we already visit this
+                if(isEverVisited):
                     continue
+
                 # never visited so uh we add to queue
                 # check if each side is equal
                 isComplete = check_side_equalness(move_state)
                 if(isComplete):
-                    heuristic_dict[move_state] = 0
+                    arr_hdict[id_h][move_state] = 0
                     openQue.put((move_state, 0))
                 else:
-                    heuristic_dict[move_state] = depth
+                    arr_hdict[id_h][move_state] = depth
                     openQue.put((move_state, depth))
-
+                # update total in dict
+                total_in_dict+=1
+                # switch to next dict if more than 1800000
+                id_h = total_in_dict > 1800000
                 
 
             
@@ -56,13 +71,14 @@ def bfs_gen():
     
     except KeyboardInterrupt:
         pass
-
-    with open('BFS_Heuristic_BY.pickle', 'wb') as handle:
-        pickle.dump(heuristic_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    for i in range(2):
+        with open(f'BFS_Heuristic_GR_{i}.pickle', 'wb') as handle:
+            pickle.dump(arr_hdict[i], handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     print("finished")
-    print(f"got: {len(heuristic_dict)} states")
-    print(Counter(heuristic_dict.values()))
+    print(f"got: {len(arr_hdict[0]) + len(arr_hdict[1])} states")
+    # print(Counter(heuristic_dict.values()))
 
 
 if __name__=="__main__":
